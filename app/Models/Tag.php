57b73db9b2603a5211444a177278acb2dd466ff5
basma123456 <72777076+basma123456@users.noreply.github.com>
2022-07-03 05:15:56 +0200

    Update Tag.php

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,9 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use App\abstracts\UnicodeModel;
 
-class Tag extends Model
+class Tag extends UnicodeModel
 {
     use HasFactory;
     protected $guarded = [];
